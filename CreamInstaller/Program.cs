df44b638f45d9eac0eb37ef1a698c8ac144e630a
HvTcCore <191157967+HvTcCore@users.noreply.github.com>
2025-11-20 11:21:42 +0700

    Updates merged from parent repo

--- CreamInstaller/Program.cs
+++ CreamInstaller/Program.cs
@@ -19,7 +19,7 @@ internal static class Program
             ? index
             : Application.ProductVersion.Length)];
 
-    internal const string RepositoryOwner = "FroggMaster";
+    internal const string RepositoryOwner = "HvTcCore";
     internal static readonly string RepositoryName = Name;
     internal static readonly string RepositoryPackage = Name + ".zip";
     internal static readonly string RepositoryExecutable = Name + ".exe";
